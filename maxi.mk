e6321a3f7fa8ccf7b81e84584597443f20f91606
Krizthian <cristianortizmorales2013@gmail.com>
2015-12-04 19:03:21 -0500

    falcon: Initial bring up to MaxiCM
    MaxiCM builds are comming soon.
    
    Signed-off-by: Krizthian <cristianortizmorales2013@gmail.com>

---
+++ maxi.mk
@@ -0,0 +1,9 @@
+$(call inherit-product, device/motorola/falcon/maxi_falcon.mk)
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/maxi/config/common_full_phone.mk)
+
+PRODUCT_RELEASE_NAME := MOTO G
+PRODUCT_NAME := maxi_falcon
+
+PRODUCT_GMS_CLIENTID_BASE := android-motorola
