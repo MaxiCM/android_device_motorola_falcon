8585028b6713c5734485eece4cd6170e76b52c0c
Krizthian <cristianortizmorales2013@gmail.com>
2015-12-04 19:19:09 -0500

    falcon: Introduce support for OTA Updater
    With this change, the falcon builds can get now a build by MaxiCM OTA Updater.
    
    Signed-off-by: Krizthian <cristianortizmorales2013@gmail.com>

--- device.mk
+++ device.mk
@@ -33,5 +33,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_wlan_nv.bin:system/etc/firmware/wlan/prima/WCNSS_qcom_wlan_nv.bin
 
+# OTA
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.ota.manifest=http://maxicm.com/falcon/ota.xml
+
 # Inherit from msm8226-common
 $(call inherit-product, device/motorola/msm8226-common/msm8226.mk)
