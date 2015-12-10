1f9c4e3b4b8fc8f41fe2d74566063d1155f787c4
Cristian Ortiz <cristian.2522@hotmail.com>
2015-12-09 21:20:19 -0500

    falcon: Introduce lines needed for MaxiCM
    MaxiCM Source need this lines for complete the build:
    
    PRODUCT_MODEL
    PRODUCT_DEVICE
    
    Adding this lines the build can complete, i don't know why falcon has this issue with the MaxiCM builds.
    
    The source cannot found in build.prop ro.product.device and ro.product.model, for that reason the build stop in the preparation of OTA Package. Adding his lines, the build can be complete.
    
    Signed-off-by: Krizthian <cristianortizmorales2013@gmail.com>

--- maxi.mk
+++ maxi.mk
@@ -5,5 +5,7 @@ $(call inherit-product, vendor/maxi/config/common_full_phone.mk)
 
 PRODUCT_RELEASE_NAME := MOTO G
 PRODUCT_NAME := maxi_falcon
+PRODUCT_MODEL := falcon
+PRODUCT_DEVICE := falcon
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
